1bddd2ddb03adcc3ab63fe5e4ef60241fa7808e5
gp22 <gp22@tutanota.com>
2016-12-30 15:27:03 -0800

    add number to string function

---
+++ numberToString/default.js
@@ -0,0 +1,16 @@
+// We need a function that can transform a number into a string.
+//
+// What ways of achieving this do you know?
+// Examples:
+//
+// numberToString(123); // returns '123';`
+// numberToString(999); // returns '999';`
+
+"use strict";
+
+function numberToString(num) {
+    // return num.toString();
+    // return num.toFixed();
+    // return num.toPrecision();
+    return String(num);
+}
