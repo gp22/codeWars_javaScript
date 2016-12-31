468829f7586ea4294b45548f845ae37268219755
gp22 <gp22@tutanota.com>
2016-12-30 16:22:12 -0800

    add shortestWord function

---
+++ shortestWord/default.js
@@ -0,0 +1,12 @@
+// x Simple, given a string of words, return the length of the shortest word(s).
+//
+// String will never be empty and you do not need to account for different data
+// types.
+
+'use strict';
+
+function findShort(s){
+  return s.split(' ').reduce(function(a, b) {
+    return (a.length < b.length) ? a : b;
+  }).length;
+}
